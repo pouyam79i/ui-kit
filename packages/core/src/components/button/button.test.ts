cae4c5d17b0205e1b5bc31edfc375e9577e75e74
Pouya <pouyamohammadyirbu@gmail.com>
2025-05-12 14:05:11 +0330

    chore: test action

--- packages/core/src/components/button/button.test.ts
+++ packages/core/src/components/button/button.test.ts
@@ -5,7 +5,6 @@ import { UIButton } from './button';
 describe('UIButton', () => {
   it('renders label', async () => {
     const el = await fixture<UIButton>(html`<ui-button label="test"></ui-button>`);
-    console.log(el.shadowRoot);
     await el.updateComplete;
     await new Promise(requestAnimationFrame);
     const shadowContent = el?.shadowRoot?.textContent;
